be35e3ffc60374b17f32033510bafc17172d0f0e
maxpme <karol5@inbox.ru>
2021-11-02 09:08:45 +0500

    color particles

--- Assets/vfx_nuitrack/Scripts/FrameData.cs
+++ Assets/vfx_nuitrack/Scripts/FrameData.cs
@@ -6,9 +6,10 @@ using Vector3 = UnityEngine.Vector3;
 
 public class FrameData : MonoBehaviour
 {
-    [SerializeField] private int scale=1000;
+    [SerializeField] private int scale = 1000;
     [SerializeField] private int width = 640;
     [SerializeField] private int height = 480;
+    [SerializeField] private RenderTexture colorMap;
     [SerializeField] private RenderTexture positionMap;
     [SerializeField] private float threshold = 4;
     [SerializeField] private int scaleZ = 1000;
@@ -17,13 +18,14 @@ public class FrameData : MonoBehaviour
     private int texHeight;
     private Color data;
 
+    Texture2D texColors;
     Texture2D texPositions;
 
 
 
 
     private List<Vector3> _positions = new List<Vector3>();
-    private Color[] _colors = new Color[640*480];
+    private Color[] _colors = new Color[640 * 480];
 
     public List<Vector3> Positions { get => _positions; private set => _positions = value; }
     public Color[] Colors { get => _colors; private set => _colors = value; }
@@ -31,48 +33,48 @@ public class FrameData : MonoBehaviour
 
     private void Start()
     {
+        NuitrackManager.onColorUpdate += GetColorFrame;
         NuitrackManager.onDepthUpdate += GetDepthFrame;
-        //NuitrackManager.onColorUpdate += GetColorFrame;
 
+        texColors = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
         texPositions = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
 
         texWidth = texPositions.width;
         texHeight = texPositions.height;
     }
-    
+
     private void GetColorFrame(ColorFrame cf)
     {
-        Colors = null;
+        //Colors = null;
         Color currentColor = new Color();
         int index = 0;
         int height = cf.Rows;
         int width = cf.Cols;
-        for(int y = 0; y < height; y++)
+        for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                currentColor = new Color32(cf[x, y].Red, cf[x, y].Green, cf[x, y].Blue, 255);
-                Colors[index] = currentColor;
+                currentColor = new Color32(cf[y, x].Red, cf[y, x].Green, cf[y, x].Blue, 255);
+                texColors.SetPixel(x, y, currentColor);
                 index++;
             }
         }
+        texColors.Apply();
+
+        RenderTexture.active = colorMap;
+        Graphics.Blit(texColors, colorMap);
     }
     private void GetDepthFrame(DepthFrame df)
     {
-        Positions.Clear();
-        Vector3 _positionCoords = new Vector3();
+        //Positions.Clear();
 
-        
         int height = df.Rows;
         int width = df.Cols;
-
-        Vector3 _projCoords=new Vector3();
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-
                 if (df[y, x] < 0.1f || df[y, x] > threshold * scale)
                 {
                     data = new Color(0.0f, 0, 0, 0.0f);
@@ -83,7 +85,7 @@ public class FrameData : MonoBehaviour
                     //_positionCoords = NuitrackManager.DepthSensor.ConvertProjToRealCoords(x, y, df[y, x]).ToVector3();
 
                     //Positions.Add(_positionCoords);
-                    data = new Color((float)x/scale,-(float)y / scale, (float)df[y, x] / scaleZ, 1.0f);
+                    data = new Color((float)x / scale, -(float)y / scale, (float)df[y, x] / scaleZ, 1.0f);
                 }
                 //texColor.SetPixel(x, y, colors[index]);
                 texPositions.SetPixel(x, y, data);
