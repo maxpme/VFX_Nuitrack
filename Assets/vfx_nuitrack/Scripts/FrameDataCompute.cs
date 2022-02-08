0bfe74b6b7f38835622c5383511c627702e564c6
maxpme <karol5@inbox.ru>
2022-02-08 11:30:29 +0500

    working

--- Assets/vfx_nuitrack/Scripts/FrameDataCompute.cs
+++ Assets/vfx_nuitrack/Scripts/FrameDataCompute.cs
@@ -1,14 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using nuitrack;
-using Vector3 = UnityEngine.Vector3;
 using System.Runtime.InteropServices;
-using System;
-using UnityEngine.UI;
-using System.Threading.Tasks;
-using System.Reflection;
-using UnityEngine.Experimental.Rendering;
 using UnityEngine.Serialization;
 
 public class FrameDataCompute : MonoBehaviour
@@ -17,24 +9,20 @@ public class FrameDataCompute : MonoBehaviour
     [Header("Input Resolution")]
     [SerializeField] private int _width = 640;
     [FormerlySerializedAs("height")] [SerializeField] private int _height = 480;
-    
+
     [Header("OutputTextures")]
     [FormerlySerializedAs("colorMap")] [SerializeField] private RenderTexture _colorMap;
     [FormerlySerializedAs("positionMap")] [SerializeField] private RenderTexture _positionMap;
+    
+    [Header("Params")]
+    [SerializeField] private float _threshold = 2000.0f;
 
     [Header("Compute Shaders Section")]
     [SerializeField] private ComputeShader _colorCompute;
     [SerializeField] private ComputeShader _positionCompute;
 
-    //test
-    [SerializeField] private RenderTexture TestMap;
-    [SerializeField]
-    private Material testMaterial;
-    //
-
     private int _texWidth;
     private int _texHeight;
-    private Color32 _data;
 
     private ComputeBuffer _colorBuffer;
     private ComputeBuffer _positionBuffer;
@@ -49,34 +37,26 @@ public class FrameDataCompute : MonoBehaviour
     private Texture2D _texColors;
     private Texture2D _texPositions;
 
-
-    private List<Vector3> _positions = new List<Vector3>();
-
-    private byte[] _colorBytes = new byte[640*480*3];
-    private uint[] _colorsArrInt = new uint[640 * 480];
-    private Color[] _positionsArray = new Color[640 * 480];
-
-    public List<Vector3> Positions { get => _positions; private set => _positions = value; }
+    private Texture2D _rawColorTexture;
+    
+    /*utils*/
+    private byte[] _depthData;
     
     private void Start()
     {
         Initialize();
-        
-        _texWidth = _texPositions.width;
-        _texHeight = _texPositions.height;
-        
-        // _tempTestMap = new RenderTexture(640, 480, 0, TestMap.format);
-        // _tempTestMap.enableRandomWrite = true;
-        // _tempTestMap.Create();
-        
     }
 
     private void Initialize()
     {
-        SubscribeToFrameData();
         CreateOutputTextures();
         CreateTempTextures();
         CreateComputerBuffers();
+        
+        SubscribeToFrameData();
+        
+        _texWidth = _texPositions.width;
+        _texHeight = _texPositions.height;
     }
 
     private void CreateComputerBuffers()
@@ -107,82 +87,91 @@ public class FrameDataCompute : MonoBehaviour
         NuitrackManager.onColorUpdate += GetColorFrame;
         NuitrackManager.onDepthUpdate += GetDepthFrame;
     }
-
-    static MethodInfo _method;
-    static object[] _args5 = new object[5];
-    public static void SetUnmanagedData
-            (ComputeBuffer buffer, IntPtr pointer, int count, int stride)
-    {
-        if (_method == null)
-        {
-            _method = typeof(ComputeBuffer).GetMethod(
-                "InternalSetNativeData",
-                BindingFlags.InvokeMethod |
-                BindingFlags.NonPublic |
-                BindingFlags.Instance
-            );
-        }
-
-        _args5[0] = pointer;
-        _args5[1] = 0;      // source offset
-        _args5[2] = 0;      // buffer offset
-        _args5[3] = count;
-        _args5[4] = stride;
-
-        _method.Invoke(buffer, _args5);
-    }
-
+    
     private void GetColorFrame(ColorFrame cf)
     {
-        byte[] currentByte= new byte[4];
-        int index = 0;
+        _rawColorTexture=new Texture2D(cf.Cols, cf.Rows, TextureFormat.RGB24, false);
         
-        Texture2D rawColorTexture = new Texture2D(cf.Cols, cf.Rows, TextureFormat.RGB24, false);
-        rawColorTexture.LoadRawTextureData(cf.Data, cf.DataSize);
-        rawColorTexture.Apply();
-        testMaterial.mainTexture = rawColorTexture;
+        //TODO: encapsulate Compute to class
+        ColorDataLoad(_rawColorTexture, cf);
 
+        int computeKernel=GetColorComputeKernel();
+        SetColorComputeVariables(computeKernel,_tempColorMap,_rawColorTexture);
+        RunColorCompute(computeKernel);
+    }
+
+    private void ColorDataLoad(Texture2D texture, ColorFrame cf)
+    {
+        texture.LoadRawTextureData(cf.Data, cf.DataSize);
+        texture.Apply();
+    }
+
+    private int GetColorComputeKernel()
+    {
         int colorComputeKernel = _colorCompute.FindKernel("ColorMain");
-        _colorCompute.GetKernelThreadGroupSizes(colorComputeKernel, out _threadGroupSizeX,
+        return colorComputeKernel;
+    }
+
+    private void SetColorComputeVariables(int kernel, RenderTexture tempTexture, Texture2D colorData)
+    {
+        _colorCompute.SetTexture(kernel, "ColorMap", tempTexture);
+        _colorCompute.SetTexture(kernel, "RawTexture", colorData);
+    }
+
+    private void RunColorCompute(int kernel)
+    {
+        _colorCompute.GetKernelThreadGroupSizes(kernel, out _threadGroupSizeX,
             out _threadGroupSizeY, out _);
         
-        _colorCompute.SetTexture(colorComputeKernel, "ColorMap", _tempColorMap);
-
-        _colorCompute.SetTexture(colorComputeKernel, "TestMap", _tempTestMap);
-
-        _colorCompute.SetTexture(colorComputeKernel, "RawTexture", rawColorTexture);
-
         var threadGroupsX = (int)(_texWidth / _threadGroupSizeX);
         var threadGroupsY = (int)(_texHeight / _threadGroupSizeY);
-        _colorCompute.Dispatch(colorComputeKernel, threadGroupsX, threadGroupsY, 1);
+        _colorCompute.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
 
         Graphics.CopyTexture(_tempColorMap, _colorMap);
-        Graphics.CopyTexture(_tempTestMap, TestMap);
-
     }
 
     private void GetDepthFrame(DepthFrame df)
     {
-        ComputeBuffer depthBuffer = new ComputeBuffer(df.DataSize / 2, sizeof(uint));
-        byte[] depthData = new byte[df.DataSize];
+        _positionBuffer=new ComputeBuffer(df.DataSize / 2, sizeof(uint));
         
+        LoadDepthData(df);
+
         int positionComputeKernel = _positionCompute.FindKernel("PositionMain");
-        _positionCompute.GetKernelThreadGroupSizes(positionComputeKernel, out _threadGroupSizeX,
-            out _threadGroupSizeY, out _);
         
-        _positionCompute.SetTexture(positionComputeKernel, "PositionMap", _tempPositionMap);
+        SetDepthComputeVariables(positionComputeKernel,_tempPositionMap, _positionBuffer, _threshold);
 
-        _positionCompute.SetBuffer(positionComputeKernel, "DepthBuffer", depthBuffer);
+        SetDataToBuffer(_depthData, _positionBuffer);
 
-        _positionCompute.SetFloat("DepthThreshold", 2000.0f);
+        RunDepthCompute(positionComputeKernel);
+    }
 
-        Marshal.Copy(df.Data, depthData, 0, depthData.Length);
+    private void LoadDepthData(DepthFrame df)
+    {
+        _depthData = new byte[df.DataSize];
+        
+        Marshal.Copy(df.Data, _depthData, 0, _depthData.Length);
+    }
 
-        depthBuffer.SetData(depthData);
+    private void SetDepthComputeVariables(int kernel, RenderTexture positionTexture, ComputeBuffer positionBuffer, float threshold)
+    {
+        _positionCompute.SetTexture(kernel, "PositionMap", positionTexture);
+        _positionCompute.SetBuffer(kernel, "DepthBuffer", positionBuffer);
+        _positionCompute.SetFloat("DepthThreshold", threshold);
+    }
+
+    private void SetDataToBuffer(byte[] data, ComputeBuffer buffer)
+    {
+        buffer.SetData(data);
+    }
+
+    private void RunDepthCompute(int kernel)
+    {
+        _positionCompute.GetKernelThreadGroupSizes(kernel, out _threadGroupSizeX,
+            out _threadGroupSizeY, out _);
         
         var threadGroupsX = (int)(640 / _threadGroupSizeX);
         var threadGroupsY = (int)(480 / _threadGroupSizeY);
-        _positionCompute.Dispatch(positionComputeKernel, threadGroupsX, threadGroupsY, 1);
+        _positionCompute.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
         /**/
         Graphics.CopyTexture(_tempPositionMap, _positionMap);
     }
